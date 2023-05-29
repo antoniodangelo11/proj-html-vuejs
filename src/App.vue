510a68c46882bb6c586c7f2c75c428059d905f39
antoninodangelo11 <reapercsp@gmail.com>
2023-05-29 14:15:49 +0200

    add structure, components, assets, and add favi..

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap";
+</style>
